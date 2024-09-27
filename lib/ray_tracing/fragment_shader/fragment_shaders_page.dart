df6bb4bf8cf83c3872f4650901afe3075d9a6168
Youssef El Behi <youssef.elbehi@gmail.com>
2024-09-28 01:14:27 +0200

    feat: add a basic fragment shader

--- lib/ray_tracing/fragment_shader/fragment_shaders_page.dart
+++ lib/ray_tracing/fragment_shader/fragment_shaders_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class FragmentShadersPage extends StatefulWidget {
@@ -8,15 +10,63 @@ class FragmentShadersPage extends StatefulWidget {
 }
 
 class _FragmentShadersPageState extends State<FragmentShadersPage> {
+  bool isLoading = true;
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+  }
+
+  Future<FragmentShader> _loadShaderFrag() async {
+    FragmentProgram program =
+        await FragmentProgram.fromAsset('shaders/ray_tracer.frag');
+    setState(() {
+      isLoading = false;
+    });
+    return program.fragmentShader();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fragment Shaders'),
       ),
-      body: const Center(
-        child: Text('Fragment Shaders'),
-      ),
+      body: Center(
+          child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: FutureBuilder<FragmentShader>(
+            future: _loadShaderFrag(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final shader = snapshot.data!;
+                return CustomPaint(
+                  painter: ShaderPainter(shader),
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            }),
+      )),
     );
+  }
+}
+
+class ShaderPainter extends CustomPainter {
+  final FragmentShader shader;
+
+  ShaderPainter(this.shader);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      Paint()..shader = shader,
+    );
+  }
+
+  @override
+  bool shouldRepaint(ShaderPainter oldDelegate) {
+    return oldDelegate.shader != shader;
   }
 }
---
+++ shaders/ray_tracer.frag
@@ -0,0 +1,5 @@
+out vec4 fragColor;
+
+void main() {
+	fragColor = vec4(0.318,0.373,1.000,1.000);
+}
